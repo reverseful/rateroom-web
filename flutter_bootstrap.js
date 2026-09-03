cd3caf0eff9816507b14e8405b8d1827db1cb6b4
reverseful <eduardocareyvmhr@hotmail.com>
2026-09-04 00:31:03 +0300

    Equalizer in listening-now strip, uniform appearance profile buttons

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2717452693" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2632225316" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
